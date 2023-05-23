f7c67928ea0e09f5bda67b4f4257c2fda9d16b35
TienDat0410 <dat010401@gmail.com>
2023-05-23 09:40:34 +0700

    first common

---
+++ src/main/java/com/example/movietdapi/MovieTdapiApplication.java
@@ -0,0 +1,14 @@
+package com.example.movietdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTdapiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MovieTdapiApplication.class, args);
+    }
+
+}
