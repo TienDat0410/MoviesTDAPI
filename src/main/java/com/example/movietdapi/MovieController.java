99a6c067c6c26fba3f977d0446cdd968a6d2bfd2
TienDat0410 <dat010401@gmail.com>
2023-05-24 16:51:26 +0700

    fix cors

--- src/main/java/com/example/movietdapi/MovieController.java
+++ src/main/java/com/example/movietdapi/MovieController.java
@@ -1,23 +1,24 @@
 package com.example.movietdapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
+/*@ComponentScan(basePackages = "com.example.movietdapi.enableCORS")*/
 public class MovieController {
 
     @Autowired
     private MovieService movieService;
+
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.finfAllMovies(), HttpStatus.OK);
---
+++ src/main/java/com/example/movietdapi/enableCORS/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.movietdapi.enableCORS;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Update with your allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
