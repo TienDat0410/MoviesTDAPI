7130ae095eeb5694f4be52b8a7ba3cbf60e0a29a
TienDat0410 <dat010401@gmail.com>
2023-05-30 09:10:51 +0700

    add CORS

--- src/main/java/com/example/movietdapi/enableCORS/CorsConfig.java
+++ src/main/java/com/example/movietdapi/enableCORS/CorsConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*@Configuration
-@EnableWebMvc*/
+@Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-  /*  @Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
-    }*/
+    }
 }
