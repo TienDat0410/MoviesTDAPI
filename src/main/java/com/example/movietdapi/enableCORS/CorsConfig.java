5d9070ecdad59a4592eaa8f6f2bed068a3c35f44
TienDat0410 <dat010401@gmail.com>
2023-05-30 13:16:56 +0700

    redeploy

--- src/main/java/com/example/movietdapi/enableCORS/CorsConfig.java
+++ src/main/java/com/example/movietdapi/enableCORS/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("*", "https://td-movies.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
